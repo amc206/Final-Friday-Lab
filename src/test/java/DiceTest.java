7af8f61e915733a968bc0fe9cad26b212d0555f3
angelmonique <campos.angel@student.greenriver.edu>
2023-03-17 12:50:28 -0700

    committing initial files

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,25 @@
+import org.example.dice.Dice;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DiceTest
+{
+    @Test
+    public void colorTest()
+    {
+        /*Dice testDice = new Dice(1, "red");
+
+        assertAll(
+                () -> assertEquals(1, "red"),
+                () -> assertEquals(4, "green")
+        );*/
+    }
+
+    @Test
+    public void sidesTest()
+    {
+
+    }
+}
