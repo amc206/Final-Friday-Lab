37e05c3afaffa3fadb4ee33cb184a57d763dc09a
angelmonique <campos.angel@student.greenriver.edu>
2023-03-17 13:29:39 -0700

    updated yml

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,19 +1,22 @@
 import org.example.dice.Dice;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ls.LSOutput;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiceTest
 {
+    //Dice testDice = new Dice(1, "red");
+
     @Test
     void getSides()
     {
-
     }
 
     @Test
-    void getColor() {
+    void getColor()
+    {
     }
 
     @Test
